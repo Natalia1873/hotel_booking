d3c99d601f87280fc35dd6fa3ce349d08177d266
Natalia Tsyleva <tsylevanatalia@gmail.com>
2026-01-02 19:39:48 +0100

    feat: add hotel REST controller with full CRUD support

---
+++ src/main/java/com/example/hotel_booking/controller/HotelController.java
@@ -0,0 +1,66 @@
+package com.example.hotel_booking.controller;
+
+import com.example.hotel_booking.model.dto.HotelPageResponseDto;
+import com.example.hotel_booking.model.dto.HotelRequestDto;
+import com.example.hotel_booking.model.dto.HotelResponseDto;
+import com.example.hotel_booking.service.HotelService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/hotels")
+@Slf4j
+public class HotelController {
+
+    private final HotelService service;
+
+    @GetMapping
+    public ResponseEntity<HotelPageResponseDto> getAllHotels(
+            @PageableDefault(size=10, sort = "name") Pageable pageable){
+
+        log.info("Fetching hotels page={}, size={}", pageable.getPageNumber(), pageable.getPageSize());
+
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HotelResponseDto> getHotelById(@PathVariable Long id){
+        log.info("Fetching hotel with id={}", id);
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<HotelResponseDto> createHotel(@Valid @RequestBody HotelRequestDto request){
+        log.info("Creating hotel: {}", request.getName());
+
+        HotelResponseDto response = service.create(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HotelResponseDto> updateHotel(@PathVariable Long id, @Valid @RequestBody HotelRequestDto request){
+
+        log.info("Updating hotel with id={}", id);
+
+        return ResponseEntity.ok(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>  deleteHotel(@PathVariable Long id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
