5af03d3f414210f0b06fd3da73d5230aca237a82
Natalia Tsyleva <tsylevanatalia@gmail.com>
2026-01-02 21:05:08 +0100

    feat: add RoomRequestDto and RoomResponseDto

--- src/main/java/com/example/hotel_booking/model/dto/RoomRequestDto.java
+++ src/main/java/com/example/hotel_booking/model/dto/RoomRequestDto.java
@@ -1,2 +1,37 @@
-package com.example.hotel_booking.model.dto;public class RoomRequestDto {
+package com.example.hotel_booking.model.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class RoomRequestDto {
+
+    @NotBlank(message = "Name не может быть пустым")
+    @Size(min=2, max = 50, message = "Name должен быть от 2 до 50 символов")
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @Positive
+    private Integer number;
+
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    @NotNull
+    @Positive
+    private Integer maxPeople;
+
+    @FutureOrPresent
+    private LocalDate occupiedFrom;
+
+    @Future
+    private LocalDate occupiedTo;
+
 }
--- src/main/java/com/example/hotel_booking/model/dto/RoomResponseDto.java
+++ src/main/java/com/example/hotel_booking/model/dto/RoomResponseDto.java
@@ -1,2 +1,19 @@
-package com.example.hotel_booking.model.dto;public class RoomResponseDto {
+package com.example.hotel_booking.model.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class RoomResponseDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Integer number;
+    private BigDecimal price;
+    private Integer maxPeople;
+    private LocalDate occupiedFrom;
+    private LocalDate occupiedTo;
+    private Long hotelId;
 }
